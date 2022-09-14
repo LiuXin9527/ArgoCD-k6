45af0b4913f1d6bdcf47aa47b6027eefd2e03272
jackliu <jack.liu@linecorp.com>
2022-09-14 15:44:25 +0800

    save2

---
+++ bases/k6-tests/test-info-apis.js
@@ -0,0 +1,54 @@
+import http from 'k6/http';
+import { group, check } from 'k6';
+
+export const options = {
+  thresholds: {
+    'checks': ['rate == 1'],
+  },
+};
+
+export default function () {
+
+  const host = __ENV.HOST || 'http://localhost:8080';
+  const token = __ENV.AUTH_TOKEN || '_Ed6edFW27*MB!BR.3ghJbHT2ZpBcj3vKW.WNj49Ao-NBe2WMHr';
+  const apiVer = __ENV.API_VERSION || 'v1';
+  const baseUrl = `${host}/api/${apiVer}`;
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': token,
+      'X-Lcp-Settle-Api-Version': apiVer,
+    },
+  };
+
+  const checkResultNotEmpty = (res) => {
+    return typeof res.json().result !== 'undefined';
+  };
+
+  group('GET /merchant/status/{sellerId}', () => {
+
+    group('Should return 200 with result approved', () => {
+      const sellerId = 500001919;
+      const res = http.get(`${baseUrl}/merchant/status/${sellerId}`, params);
+
+      check(res, {
+        'status is 200': (r) => r.status === 200,
+        'success is true': (r) => r.json().success === true,
+        'status is approved': (r) => checkResultNotEmpty(r) && r.json().result.status === "APPROVED",
+      });
+    });
+
+    group('Should return 200 with result not registered', () => {
+      const sellerId = -1;
+      const res = http.get(`${baseUrl}/merchant/status/${sellerId}`, params);
+
+      check(res, {
+        'status is 200': (r) => r.status === 200,
+        'success is true': (r) => r.json().success === true,
+        'status is not registered': (r) => checkResultNotEmpty(r) && r.json().result.status === "DO_NOT_REGISTER",
+      });
+    });
+
+  });
+
+}
